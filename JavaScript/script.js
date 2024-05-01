3115068eede8cd69b83c41378c9ba3a964e2db3e
jmcastanos <git@webinver.com>
2024-04-30 20:17:34 -0700

    Add weather functionality to JavaScript weather app

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,9 +9,34 @@ form.addEventListener('submit', function(e) {
     let location = document.getElementById('city').value;
     
     console.log(location);
+    getWeather(location);
+    
 
+});
+navigator.geolocation.getCurrentPosition((position) => {
+    getWeather("", position.coords.latitude, position.coords.longitude);
+});
+
+function getGeoIp(){
+    fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let city = data.city;
+        //let country = data.country_name;
+        let lat = data.latitude;
+        let lon = data.longitude;
+        getWeather(city, lat, lon);
+    });
+
+}
+
+function getWeather(location, lat, lon){
     let apiKey = '{API_KEY}';
     let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lang=es&q=' + location + '&appid=' + apiKey;
+    if(typeof lat !== 'undefined' && typeof lon !== 'undefined'){
+        apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lang=es&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+    }
 
     fetch(apiUrl)
     .then(response => response.json())
@@ -28,6 +53,6 @@ form.addEventListener('submit', function(e) {
         document.getElementById('temperature').textContent = Math.round(temperature - 273.15) + '°C';
         document.getElementById('description').textContent = description;
         document.getElementById('icon').src = 'http://openweathermap.org/img/w/' + icon + '.png';
+        
     });
-
-});
+}
